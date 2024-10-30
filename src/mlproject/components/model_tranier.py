4c237bc77ab7789990c48ca46cf52788ac7f6d24
1602saurab <saurabhsonibrd@gmail.com>
2024-10-30 15:16:52 +0530

    ml model and its score found

--- src/mlproject/components/model_tranier.py
+++ src/mlproject/components/model_tranier.py
@@ -1,11 +1,11 @@
 import os
 import sys
 from dataclasses import dataclass
-#from urllib.parse import urlparse
-# import mlflow
-# import mlflow.sklearn
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
 import numpy as np
-from sklearn.metrics import mean_squared_error,mean_absolute_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
@@ -20,31 +20,33 @@ from xgboost import XGBRegressor
 
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
-from src.mlproject.utils import save_object,evaluate_models
+from src.mlproject.utils import save_object, evaluate_models
 
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path=os.path.join("artifacts","model.pkl")
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
 
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config=ModelTrainerConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
-    # def eval_metrics(self,actual, pred):
-    #     rmse = np.sqrt(mean_squared_error(actual, pred))
-    #     mae = mean_absolute_error(actual, pred)
-    #     r2 = r2_score(actual, pred)
-    #     return rmse, mae, r2
+    # Define the eval_metrics function
+    def eval_metrics(self, actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
 
-    def initiate_model_trainer(self,train_array,test_array):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("Split training and test input data")
-            X_train,y_train,X_test,y_test=(
-                train_array[:,:-1],
-                train_array[:,-1],
-                test_array[:,:-1],
-                test_array[:,-1]
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1],
             )
             models = {
                 "Random Forest": RandomForestRegressor(),
@@ -55,50 +57,39 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
-            params={
+            params = {
                 "Decision Tree": {
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    # 'splitter':['best','random'],
-                    # 'max_features':['sqrt','log2'],
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
                 },
-                "Random Forest":{
-                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                 
-                    # 'max_features':['sqrt','log2',None],
-                    'n_estimators': [8,16,32,64,128,256]
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-                "Gradient Boosting":{
-                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                    'learning_rate':[.1,.01,.05,.001],
-                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    # 'criterion':['squared_error', 'friedman_mse'],
-                    # 'max_features':['auto','sqrt','log2'],
-                    'n_estimators': [8,16,32,64,128,256]
+                "Gradient Boosting": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-                "Linear Regression":{},
-                "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
+                "Linear Regression": {},
+                "XGBRegressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-                "CatBoosting Regressor":{
-                    'depth': [6,8,10],
+                "CatBoosting Regressor": {
+                    'depth': [6, 8, 10],
                     'learning_rate': [0.01, 0.05, 0.1],
                     'iterations': [30, 50, 100]
                 },
-                "AdaBoost Regressor":{
-                    'learning_rate':[.1,.01,0.5,.001],
-                    # 'loss':['linear','square','exponential'],
-                    'n_estimators': [8,16,32,64,128,256]
+                "AdaBoost Regressor": {
+                    'learning_rate': [.1, .01, 0.5, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
                 }
-                
             }
-            model_report:dict=evaluate_models(X_train,y_train,X_test,y_test,models,params)
+            model_report: dict = evaluate_models(X_train, y_train, X_test, y_test, models, params)
 
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
-             ## To get best model name from dict
-
+            ## To get best model name from dict
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
@@ -109,7 +100,7 @@ class ModelTrainer:
 
             model_names = list(params.keys())
 
-            actual_model=""
+            actual_model = ""
 
             for model in model_names:
                 if best_model_name == model:
@@ -121,11 +112,11 @@ class ModelTrainer:
             tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
             # mlflow
-
             with mlflow.start_run():
 
                 predicted_qualities = best_model.predict(X_test)
 
+                # Call the eval_metrics function
                 (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
 
                 mlflow.log_params(best_params)
@@ -134,22 +125,13 @@ class ModelTrainer:
                 mlflow.log_metric("r2", r2)
                 mlflow.log_metric("mae", mae)
 
-
                 # Model registry does not work with file store
                 if tracking_url_type_store != "file":
-
-                    # Register the model
-                    # There are other ways to use the Model Registry, which depends on the use case,
-                    # please refer to the doc for more information:
-                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
                     mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
                 else:
                     mlflow.sklearn.log_model(best_model, "model")
 
-
-
-
-            if best_model_score<0.6:
+            if best_model_score < 0.6:
                 raise CustomException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
 
@@ -158,12 +140,19 @@ class ModelTrainer:
                 obj=best_model
             )
 
-            predicted=best_model.predict(X_test)
+            predicted = best_model.predict(X_test)
 
             r2_square = r2_score(y_test, predicted)
             return r2_square
 
-
-
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
+
+
+if __name__ == "__main__":
+    # Replace `train_arr` and `test_arr` with your actual data
+    train_arr = np.random.rand(100, 5)
+    test_arr = np.random.rand(50, 5)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
